0a8c113f88c47e0a1532332b8531b35e02ef8a0f
kotmatross28729 <kotmatross228@gmail.com>
2025-03-21 22:47:30 +0300

    Flix

--- src/main/java/com/kotmatross/shadersfixer/mixins/late/client/hbm/client/sedna/guns/MixinItemRenderMaresleg.java
+++ src/main/java/com/kotmatross/shadersfixer/mixins/late/client/hbm/client/sedna/guns/MixinItemRenderMaresleg.java
@@ -2,17 +2,21 @@ package com.kotmatross.shadersfixer.mixins.late.client.hbm.client.sedna.guns;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.hbm.main.ResourceManager;
 import com.hbm.render.item.weapon.sedna.ItemRenderMaresleg;
 
 @Mixin(value = ItemRenderMaresleg.class, priority = 999)
 public class MixinItemRenderMaresleg {
+
+    @Shadow
+    public ResourceLocation texture;
 
     @Inject(
         method = "renderFirstPerson",
@@ -22,6 +26,6 @@ public class MixinItemRenderMaresleg {
             shift = At.Shift.AFTER),
         remap = false)
     public void fixAfterSmoke(ItemStack stack, CallbackInfo ci) {
-        Minecraft.getMinecraft().renderEngine.bindTexture(ResourceManager.maresleg_tex);
+        Minecraft.getMinecraft().renderEngine.bindTexture(texture);
     }
 }
